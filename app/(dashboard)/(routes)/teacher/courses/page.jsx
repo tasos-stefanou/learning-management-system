73ef4b8639e8a598f6d764c407ac03cf01d83918
Tasos Stefanou <tasos.stefanou.se@gmail.com>
2024-08-19 15:48:58 +0300

    display courses of teacher in courses page with data table, add search, pagination & sorting

--- app/(dashboard)/(routes)/teacher/courses/page.jsx
+++ app/(dashboard)/(routes)/teacher/courses/page.jsx
@@ -1,12 +1,29 @@
-import Link from 'next/link';
-import { Button } from '@/components/ui/button';
+import { DataTable } from './_components/DataTable';
+import { columns } from './_components/columns';
+import { redirect } from 'next/navigation';
+import { db } from '@/lib/db';
+import { auth } from '@clerk/nextjs/server';
 
-const CoursesPage = () => {
+const CoursesPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect('/');
+  }
+
+  // TODO: courses dont get updated after deleting a course
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+
   return (
     <div className='p-6'>
-      <Link href='/teacher/create'>
-        <Button>New course</Button>
-      </Link>
+      <DataTable columns={columns} data={courses} />
     </div>
   );
 };
