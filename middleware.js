f1a1775b8a31d1ff5ecbf23aea55560b1324ab2e
tasos-stefanou <tasos.stefanou.se@gmail.com>
2024-08-23 18:45:30 +0300

    allow stripe webhook route to be accessible from everyone

--- middleware.js
+++ middleware.js
@@ -6,7 +6,7 @@ const isPublicRoute = createRouteMatcher([
   '/sign-in(.*)',
   '/sign-up(.*)',
   '/api/uploadthing(.*)',
-  // '/api/webhook(.*)',
+  '/api/webhook(.*)',
 ]);
 
 export default clerkMiddleware((auth, req) => {
